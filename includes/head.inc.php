92ad54eda40cc0777be7e1e0b7a90829f3b8b186
tayloa <halfnote1004@gmail.com>
2018-04-24 19:47:57 -0400

    MOdified folder structure and made new php files to prevent repition

---
+++ includes/head.inc.php
@@ -0,0 +1,8 @@
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css"
+  integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
+  <link href="resources/style.css" rel="stylesheet" type="text/css"/>
+  <title>Traffic Tracker</title>
+</head>
+<body>
--- index.php
+++ index.php
@@ -2,18 +2,27 @@
   include('includes/init.inc.php'); // include the DOCTYPE and opening tags
   include('includes/functions.inc.php'); // functions
 ?>
-<title>PHP &amp; MySQL - ITWS 2013</title>
 
 <?php
   include('includes/head.inc.php');
   // include global css, javascript, end the head and open the body
 ?>
 
-<h1>PHP &amp; MySQL</h1>
-
-<?php include('includes/menubody.inc.php'); ?>
-
 <!-- INNER HTML WILL GO HERE -->
+<div class="content">
+  <div class="title">
+    <h1 class="logo">Traffic Tracker</h1>
+  </div>
+  <div>
+    <h3 class="sub-text">News and data on one platform</h3>
+  </div>
+</div>
+<div class="input-group mb-3" id="search-bar">
+  <input type="text" class="form-control" placeholder="Enter a url (ex: youtube.com)" aria-label="Recipient's username" aria-describedby="basic-addon2">
+  <div class="input-group-append">
+    <input class="btn btn-outline-secondary" value="Search" onclick="validate()" type="button"></input>
+  </div>
+</div>
 
 <?php include('includes/foot.inc.php');
   // footer info and closing tags
