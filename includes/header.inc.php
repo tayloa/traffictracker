10961d7b553c6946ccb88a3435ea2be11a564685
tayloa <halfnote1004@gmail.com>
2018-04-24 13:09:36 -0400

    Added header and footer for php pages

---
+++ includes/header.inc.php
@@ -0,0 +1,7 @@
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+  <script type="text/javascript" src="resources/jquery-1.4.3.min.js"></script>
+  <script type="text/javascript" src="resources/search.js"></script>
+  <link href="resources/styles.css" rel="stylesheet" type="text/css"/>
+</head>
+<body>
+  <div id="header">
