bbfa995c7e00d3227bc59943a0952fffaf6ca7d2
tayloa <halfnote1004@gmail.com>
2018-04-26 10:35:14 -0400

    Added date range validation and styling to dashboard form.

--- dashboard.php
+++ dashboard.php
@@ -41,31 +41,37 @@
   <form id="complex-search-form">
 
     <div class="row justify-content-md-center">
-      <div class="col">
+      <div class="col input-group mb-3">
         <input type="text" class="form-control" placeholder="Enter a topic"
         value="" name="query" id="query" aria-label="query" aria-describedby="basic-addon2">
+        <div class="input-group-append">
+          <a id="submit-button" tabindex="0" class="btn btn-success" role="button" data-toggle="popover" data-trigger="focus"
+          data-content="Please enter a query." data-placement="top" type="submit" onclick='$("#complex-search-form").submit()'>
+          <i class="fas fa-search"></i>
+          </a>
+        </div>
       </div>
+
     </div>
 
     <div class="form-row">
-      <!-- <div class="form-group col-md-6">
-        <input type="text" class="form-control" id="inputCity">
-      </div> -->
       <div class="form-group col-md-auto">
         <select id="inputDate" class="form-control">
           <option>Past Day</option>
           <option>Past Week</option>
           <option>Past Month</option>
           <option selected>Past 30 Days</option>
-          <option data-toggle="modal" data-target="#inputDateRange">Custom Date Range</option>
+          <option id="optionDateRange" data-toggle="modal" data-target="#inputDateRange">Custom Date Range</option>
         </select>
       </div>
-      <div class="form-group col-md-2">
+      <!-- <div class="form-group col-md-2">
         <div class="input-group-append">
           <a id="submit-button" tabindex="0" class="btn btn-success" role="button" data-toggle="popover" data-trigger="focus"
-          data-content="Please enter a query." data-placement="top" type="submit" onclick='$("#search-form").submit()'>Search</a>
+          data-content="Please enter a query." data-placement="top" type="submit" onclick='$("#complex-search-form").submit()'>
+          <i class="fas fa-search"></i>
+          </a>
         </div>
-      </div>
+      </div> -->
   </form>
 </div>
 
@@ -79,19 +85,18 @@
         </button>
       </div>
       <div class="modal-body">
-        <form>
+        <form id="date-range-form">
           <div class="form-group">
 
-            <div class="value"><input type="text" size="10" maxlength="10" value="" name="dob" id="dob"/> <em>yyyy-mm-dd</em></div>
-            <p>to</p>
-            <div class="value"><input type="text" size="10" maxlength="10" value="" name="dob" id="dob"/> <em>yyyy-mm-dd</em></div>
+            <div class="value"><input type="text" size="10" maxlength="10" value="" name="startDate" id="startDate"/> <em>yyyy-mm-dd</em></div>
+            <div class="value"><input type="text" size="10" maxlength="10" value="" name="endDate" id="endDate"/> <em>yyyy-mm-dd</em></div>
 
           </div>
         </form>
       </div>
       <div class="modal-footer">
         <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-        <button type="submit" class="btn btn-primary">Save changes</button>
+        <button type="submit" class="btn btn-primary" onclick='$("#date-range-form").submit()'>Save range</button>
       </div>
     </div>
   </div>
--- resources/dashboard.js
+++ resources/dashboard.js
@@ -1,3 +1,33 @@
+function validateDate(dateString) {
+  var regEx = /^\d{4}-\d{2}-\d{2}$/;
+  return dateString.match(regEx) != null;
+}
+
+$( "#date-range-form" ).submit(function( event ) {
+    event.preventDefault();
+    var startDate = $('#startDate');
+    var endDate = $('#endDate');
+    var fail = true;
+
+    if (!validateDate(startDate.val())) {
+      alert("Please enter a valid start date (yyyy-mm-dd)");
+      startDate.focus();
+      return false;
+    }
+    if (!validateDate(endDate.val())) {
+      alert("Please enter a valid end date (yyyy-mm-dd)");
+      endDate.focus();
+      return false;
+    }
+    else {
+      console.log(startDate,endDate);
+      var dateRange = startDate.val() + " to " + endDate.val();
+      $('#inputDateRange').modal('toggle');
+      $("#optionDateRange").text(dateRange);
+      return true;
+    }
+});
+
 $( document ).ready(function(){
 
   // $('#welcomeModal').modal({
@@ -10,7 +40,4 @@ $( document ).ready(function(){
     }
   });
 
-  $('#exampleModalLong').on('shown.bs.modal', function () {
-  // $('#myInput').trigger('focus')
-  });
 });
