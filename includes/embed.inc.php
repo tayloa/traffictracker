6987a6fac0de42acbe257456f22447b673c6169f
tayloa <halfnote1004@gmail.com>
2018-04-27 13:19:28 -0400

    Finalized design for embeded data card display on dashboard

--- includes/embed.inc.php
+++ includes/embed.inc.php
@@ -1,105 +1,61 @@
-<div class="card-deck">
-   <div class="card text-center">
-     <div class="card-header">
-         <a data-toggle="collapse" href="#trends-block" aria-expanded="true" aria-controls="trends-block">
-           <h4 class="card-title">GOOGLE TRENDS</h4>
-         </a>
-     </div>
-     <div id="trends-block" class="collapse">
-         <div class="card-block">
-           <div class="card-body">
-             <h1>GOOGLE TRENDS</h1>
+  <div class="card-deck">
+     <div class="card text-center">
+       <div class="card-header">
+           <a id="badTag" class= "card-toggle" data-toggle="collapse" href="#trends-block" aria-expanded="true" aria-controls="trends-block">
+             <h4 class="card-title"><i class="fas fa-chart-line"></i> TRENDS</h4>
+           </a>
+       </div>
+       <div id="trends-block" class="collapse">
+           <div class="card-block">
+             <div class="card-body">
+               <h1>GOOGLE TRENDS</h1>
+             </div>
            </div>
-         </div>
+       </div>
      </div>
-   </div>
-   <div class="card text-center">
-     <div class="card-header">
-         <a data-toggle="collapse" href="#twitter-block" aria-expanded="true" aria-controls="twitter-block">
-           <h4 class="card-title">TWITTER</h4>
-         </a>
-     </div>
-     <div id="twitter-block" class="collapse">
-         <div class="card-block">
-           <div class="card-body">
-             <h1>TWITTER FEED</h1>
+     <div class="card text-center">
+       <div class="card-header">
+           <a class= "card-toggle" data-toggle="collapse" href="#twitter-block" aria-expanded="true" aria-controls="twitter-block">
+             <h4 class="card-title"><i class="fab fa-twitter"></i> TWITTER</h4>
+           </a>
+       </div>
+       <div id="twitter-block" class="collapse">
+           <div class="card-block">
+             <div class="card-body">
+               <h1>TWITTER FEED</h1>
+             </div>
            </div>
-         </div>
+       </div>
      </div>
-   </div>
-</div>
-<div class="card-deck">
-   <div class="card text-center">
-     <div class="card-header">
-         <a data-toggle="collapse" href="#rss-block" aria-expanded="true" aria-controls="rss-block">
-           <h4 class="card-title">NEWS</h4>
-         </a>
-     </div>
-     <div id="rss-block" class="collapse">
-         <div class="card-block">
-           <div class="card-body">
-             <h1>RSS FEED</h1>
+  </div>
+  <div class="card-deck">
+     <div class="card text-center">
+       <div class="card-header">
+           <a class= "card-toggle" data-toggle="collapse" href="#rss-block" aria-expanded="true" aria-controls="rss-block">
+             <h4 class="card-title"><i class="fas fa-rss-square"></i> NEWS</h4>
+           </a>
+       </div>
+       <div id="rss-block" class="collapse">
+           <div class="card-block">
+             <div class="card-body">
+               <h1>RSS FEED</h1>
+             </div>
            </div>
-         </div>
+       </div>
      </div>
-   </div>
-   <div class="card text-center">
-     <div class="card-header">
-         <a data-toggle="collapse" href="#instagram-block" aria-expanded="true" aria-controls="instagram-block">
-           <h4 class="card-title">INSTAGRAM</h4>
-         </a>
-     </div>
-     <div id="instagram-block" class="collapse">
-         <div class="card-block">
-           <div class="card-body">
-             <h1>INSTAGRAM FEED</h1>
+     <div class="card text-center">
+       <div class="card-header">
+           <a class= "card-toggle" data-toggle="collapse" href="#instagram-block" aria-expanded="true" aria-controls="instagram-block">
+             <h4 class="card-title"><i class="fab fa-instagram"></i> INSTAGRAM</h4>
+           </a>
+       </div>
+       <div id="instagram-block" class="collapse">
+           <div class="card-block">
+             <div class="card-body">
+               <h1><i class="fab fa-instagram"></i>INSTAGRAM FEED</h1>
+             </div>
            </div>
-         </div>
+       </div>
      </div>
-   </div>
-</div>
-
-<!-- <div class="card">
-    <div class="card-header">
-        <a data-toggle="collapse" href="#test-block" aria-expanded="true" aria-controls="test-block">
-            card header
-        </a>
-    </div>
-    <div id="test-block" class="collapse">
-        <div class="card-block">
-            card block
-        </div>
-    </div>
-</div> -->
-  <!-- <div class="row">
-    <div class="card pull-left col-md-6">
-      <script type="text/javascript"
-      src="https://ssl.gstatic.com/trends_nrtr/1386_RC02/embed_loader.js"></script> <script type="text/javascript">
-      trends.embed.renderExploreWidget("TIMESERIES",
-      {"comparisonItem":[{"keyword":"kanye","geo":"","time":"now 7-d"}],"category":0,"property":""},
-      {"exploreQuery":"date=now%207-d&q=kanye","guestPath":"https://trends.google.com:443/trends/embed/"});
-      </script>
-      <div class="card-body">
-        <h1>GOOGLE TRENDS</h1>
-      </div>
-    </div>
-      <div class="card pull-right col-md-6">
-        <a class="twitter-timeline"  href="https://twitter.com/hashtag/Kanye" data-widget-id="989655009162924033">#Kanye Tweets</a>
-        <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
-        <div class="card-body">
-          <h1>TWITTER FEED</h1>
-        </div>
-      </div>
-    <div class="card pull-left col-md-6">
-      <div class="card-body">
-        <h1>RSS FEED</h1>
-      </div>
-    </div>
-
-    <div class="card pull-right col-md-6">
-      <div class="card-body">
-        <h1>INSTAGRAM FEED</h1>
-      </div>
-    </div>
-  </div> -->
+  </div>
 </div>
