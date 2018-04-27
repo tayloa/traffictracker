ac78d73feca06aab52ba3c762953e5e2f0cb314d
tayloa <halfnote1004@gmail.com>
2018-04-27 01:50:51 -0400

    Made small changes to cards.

--- includes/embed.inc.php
+++ includes/embed.inc.php
@@ -1,66 +1,62 @@
-<div class="container">
-  <div class="card-deck">
-     <div class="card text-center">
-       <div class="card-header">
-           <a data-toggle="collapse" href="#trends-block" aria-expanded="true" aria-controls="test-block">
-             <h4 class="card-title">Permits</h4>
-           </a>
-       </div>
-       <div id="trends-block" class="collapse">
-           <div class="card-block">
-             <div class="card-body">
-               <h1>GOOGLE TRENDS</h1>
-             </div>
-           </div>
-       </div>
+<div class="card-deck">
+   <div class="card text-center">
+     <div class="card-header">
+         <a data-toggle="collapse" href="#trends-block" aria-expanded="true" aria-controls="trends-block">
+           <h4 class="card-title">GOOGLE TRENDS</h4>
+         </a>
      </div>
-     <div class="card text-center">
-       <div class="card-header">
-           <a data-toggle="collapse" href="#twitter-block" aria-expanded="true" aria-controls="test-block">
-             <h4 class="card-title">Permits</h4>
-           </a>
-       </div>
-       <div id="twitter-block" class="collapse">
-           <div class="card-block">
-             <div class="card-body">
-               <h1>TWITTER FEED</h1>
-             </div>
+     <div id="trends-block" class="collapse">
+         <div class="card-block">
+           <div class="card-body">
+             <h1>GOOGLE TRENDS</h1>
            </div>
-       </div>
+         </div>
      </div>
- </div>
+   </div>
+   <div class="card text-center">
+     <div class="card-header">
+         <a data-toggle="collapse" href="#twitter-block" aria-expanded="true" aria-controls="twitter-block">
+           <h4 class="card-title">TWITTER</h4>
+         </a>
+     </div>
+     <div id="twitter-block" class="collapse">
+         <div class="card-block">
+           <div class="card-body">
+             <h1>TWITTER FEED</h1>
+           </div>
+         </div>
+     </div>
+   </div>
 </div>
-<div class="container">
-  <div class="card-deck">
-     <div class="card text-center">
-       <div class="card-header">
-           <a data-toggle="collapse" href="#rss-block" aria-expanded="true" aria-controls="test-block">
-             <h4 class="card-title">Permits</h4>
-           </a>
-       </div>
-       <div id="rss-block" class="collapse">
-           <div class="card-block">
-             <div class="card-body">
-               <h1>RSS FEED</h1>
-             </div>
-           </div>
-       </div>
+<div class="card-deck">
+   <div class="card text-center">
+     <div class="card-header">
+         <a data-toggle="collapse" href="#rss-block" aria-expanded="true" aria-controls="rss-block">
+           <h4 class="card-title">NEWS</h4>
+         </a>
      </div>
-     <div class="card text-center">
-       <div class="card-header">
-           <a data-toggle="collapse" href="#instagram-block" aria-expanded="true" aria-controls="test-block">
-             <h4 class="card-title">Permits</h4>
-           </a>
-       </div>
-       <div id="instagram-block" class="collapse">
-           <div class="card-block">
-             <div class="card-body">
-               <h1>INSTAGRAM FEED</h1>
-             </div>
+     <div id="rss-block" class="collapse">
+         <div class="card-block">
+           <div class="card-body">
+             <h1>RSS FEED</h1>
            </div>
-       </div>
+         </div>
      </div>
-  </div>
+   </div>
+   <div class="card text-center">
+     <div class="card-header">
+         <a data-toggle="collapse" href="#instagram-block" aria-expanded="true" aria-controls="instagram-block">
+           <h4 class="card-title">INSTAGRAM</h4>
+         </a>
+     </div>
+     <div id="instagram-block" class="collapse">
+         <div class="card-block">
+           <div class="card-body">
+             <h1>INSTAGRAM FEED</h1>
+           </div>
+         </div>
+     </div>
+   </div>
 </div>
 
 <!-- <div class="card">
